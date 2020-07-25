3ad9d8c9326a8e153ca1ffad5954b76b84610198
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-25 18:03:06 +0530

    Final static todos

--- server.js
+++ server.js
@@ -17,11 +17,18 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
+
 	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	}
 	else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return(todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1);
+		});
 	}
 
 	res.json(filteredTodos);
