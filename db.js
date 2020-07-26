8ee82ddd096b4fb7e5039450b871d508e04e7e17
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-26 18:41:24 +0530

    Add postgres to production

--- db.js
+++ db.js
@@ -1,9 +1,20 @@
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
 var path = require('path');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var sequelize;
+
+if(env === 'production') { // To conect to postgres server on heroku
+	sequelize = new Sequelize(process.env.DATABASE_URL, { 
+		dialect: 'postgres'
+	});
+}
+else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
+
 var db = {};
 
 db.todo = require(path.join(__dirname, '/models/todo.js'))(sequelize, Sequelize.DataTypes);
