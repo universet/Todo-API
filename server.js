3d3f542b10a68c34da6c993596808ac717e16488
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-26 17:27:15 +0530

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,8 +16,27 @@ app.get('/', function(req, res) {
 
 //GET /todos
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
+
+	if(query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	}
+	else if(query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	}
+	if(query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query + '%'
+		};
+	}
+	db.todo.findAll({where: where}).then(function (todos) {
+		res.json(todos);
+	}, function (e) {
+		res.status(500).send();
+	});
+
+	/*var filteredTodos = todos;
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {
@@ -36,6 +55,7 @@ app.get('/todos', function(req, res) {
 	}
 
 	res.json(filteredTodos);
+	*/
 });
 
 //GET / todo/:id
