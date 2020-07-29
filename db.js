9645662a1c750e6f699be867faa8f74886e5abbc
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-29 07:29:38 +0530

    Added DELETE /user/login

--- db.js
+++ db.js
@@ -19,6 +19,7 @@ var db = {};
 
 db.todo = require(path.join(__dirname, '/models/todo.js'))(sequelize, Sequelize.DataTypes);
 db.user = require(path.join(__dirname, '/models/user.js'))(sequelize, Sequelize.DataTypes);
+db.token = require(path.join(__dirname, '/models/token.js'))(sequelize, Sequelize.DataTypes);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
